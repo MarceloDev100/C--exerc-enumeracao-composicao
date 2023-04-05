089dd1c28effc8dd7a6b655b5facb88f2dab492b
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-05 15:38:20 -0300

    Order class and OrderStatus enumeration created

---
+++ Course/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+  Enumerações: Tipo valor representado por um conjunto de constantes relacionadas.
+  palavra chave: enum
+  -vantagem: semântica(entendimento), 
+  -código legível,
+  -auxiliado pelo compilador(evita erros).
+*/
+
+
+
+
+
